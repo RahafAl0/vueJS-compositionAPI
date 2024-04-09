5fd1b484681f8ffedc021aff92f00f823250be6e
RahafAl0 <ra.al9760@gmail.com>
2024-04-10 02:41:18 +0300

    make simple counter in options API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,37 @@
 <template>
-    <div class="home">
-        <h1>Home</h1>
-    </div>
+  <div class="home">
+    <button @click="decreaseCounter" class="btn">-</button>
+    <span class="counter">{{ counter }}</span>
+    <button @click="increaceCounter" class="btn">+</button>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increaceCounter() {
+      this.counter++;
+    },
+    decreaseCounter() {
+      this.counter--;
+    },
+  },
+};
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
